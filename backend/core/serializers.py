8bdd274b2ba4a81e54c9e194cf5e0a6af4cac182
martinsbruno2026 <martinsbru08@gmail.com>
2026-04-08 14:27:44 -0300

    feat update

--- backend/core/serializers.py
+++ backend/core/serializers.py
@@ -1,20 +1,24 @@
+
 from rest_framework import serializers
-from .models import Acessorio, Cor, Modelo, Veiculo
+from .models import *
 
 class AcessorioSerializer(serializers.ModelSerializer):
     class Meta:
         model = Acessorio
         fields = '__all__'
 
+
 class CorSerializer(serializers.ModelSerializer):
     class Meta:
         model = Cor
         fields = '__all__'
 
+
 class ModeloSerializer(serializers.ModelSerializer):
     class Meta:
         model = Modelo
         fields = '__all__'
+
 
 class VeiculoSerializer(serializers.ModelSerializer):
     class Meta:
